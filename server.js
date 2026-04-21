0ada36ce2274e108fcbf7b3b3886b4d8f8638005
Ryan <mr.yaryyev@gmail.com>
2026-04-21 15:45:04 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log('Web Serverni boshlash');
 const express = require("express");
 const app = express(); // expressning app Objectni yuboradi
 const http = require('http');
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:" ,err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 // app objectida 4 ta bosgichga bolamiz
 
@@ -35,6 +45,10 @@ app.post('/create-item', (req, res) => {
     //console.log(req.body);
     //console.log(req);
     //res.json({ test: "success" });
+});
+
+app.get('/author', (req, res) => {
+    res.render('author', {user: user});
 });
 
 app.get("/", function (req, res) {
