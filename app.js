fedafa6f216a4a6e1caf5a81d8fc88d16d41cc62
Ryan <mr.yaryyev@gmail.com>
2026-04-25 17:27:54 +0900

    feat: mongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,54 @@
+console.log('Web Serverni boshlash');
+const express = require("express");
+const app = express(); // expressning app Objectni yuboradi
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:" ,err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
+
+// MongoDB choqirish
+const db = require("./server").db();
+
+
+// app objectida 4 ta bosgichga bolamiz
+
+// 1: Kirish code
+app.use(express.static("public")); // public folder ochik boladi clientlarga/koroladi
+app.use(express.json()); // kirip kelyatgan json formatdaki datani Object holatiga bizga ogurip beradi
+app.use(express.urlencoded({extended: true})); // html'dan traditional request form shakli bor, formdan biror narsani post qilsak, bizning express serverimiz qabul qiloladi
+
+// 2: Sesssion code
+
+// 3: Views code
+// bssr - yoli tanladik, backend'de view yasaymiz
+// npm i ejs -> install qilamiz, EJS = Embedded Javascript, ejs orqali backend'da frontend'ni yosaymiz
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+
+// 4: Routing code
+
+app.post('/create-item', (req, res) => {
+    //console.log(req.body);
+    //console.log(req);
+    //res.json({ test: "success" });
+});
+
+app.get('/author', (req, res) => {
+    res.render('author', {user: user});
+});
+
+app.get("/", function (req, res) {
+    res.render("reja");
+});
+
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,53 +1,27 @@
-console.log('Web Serverni boshlash');
-const express = require("express");
-const app = express(); // expressning app Objectni yuboradi
 const http = require('http');
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:" ,err);
-    } else {
-        user = JSON.parse(data)
+
+let db;
+const connectionString = "mongodb+srv://rovshen:wHZFt7bMDXCPM0sG@cluster0.3nspg5e.mongodb.net/";
+
+mongodb.connect(connectionString, {
+    useNewUrlParsel: true,
+    useUnifiedTopology: true,
+}, (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+        console.log("MongoDB connection succeed");
+        module.exports = client;
+        //console.log(client);
+        const app = require("./app");
+        const server = http.createServer(app); //serverni http orqali qurdik
+        let PORT = 3000;
+        server.listen(PORT, function () {
+            console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+        });
+
     }
 });
 
-// app objectida 4 ta bosgichga bolamiz
 
-// 1: Kirish code
-app.use(express.static("public")); // public folder ochik boladi clientlarga/koroladi
-app.use(express.json()); // kirip kelyatgan json formatdaki datani Object holatiga bizga ogurip beradi
-app.use(express.urlencoded({extended: true})); // html'dan traditional request form shakli bor, formdan biror narsani post qilsak, bizning express serverimiz qabul qiloladi
-
-// 2: Sesssion code
-
-// 3: Views code
-// bssr - yoli tanladik, backend'de view yasaymiz
-// npm i ejs -> install qilamiz, EJS = Embedded Javascript, ejs orqali backend'da frontend'ni yosaymiz
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-
-// 4: Routing code
-
-app.post('/create-item', (req, res) => {
-    //console.log(req.body);
-    //console.log(req);
-    //res.json({ test: "success" });
-});
-
-app.get('/author', (req, res) => {
-    res.render('author', {user: user});
-});
-
-app.get("/", function (req, res) {
-    res.render("reja");
-});
-
-
-const server = http.createServer(app); //serverni http orqali qurdik
-let PORT = 3000;
-server.listen(PORT, function () {
-    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-});
