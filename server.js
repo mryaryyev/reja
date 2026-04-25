bf8d366a5ec2e9de99fd6b4b3d8ef440861b3083
Ryan <mr.yaryyev@gmail.com>
2026-04-25 16:22:25 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -42,12 +42,12 @@ app.get('/author', (req, res) => {
 });
 
 app.get("/", function (req, res) {
-    res.render("harid");
+    res.render("reja");
 });
 
 
 const server = http.createServer(app); //serverni http orqali qurdik
 let PORT = 3000;
 server.listen(PORT, function () {
-    console.log(`The server is running successfully on port: ${PORT}`);
+    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
 });
